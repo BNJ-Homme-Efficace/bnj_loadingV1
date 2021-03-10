50460c9348853a17207a1669bd42195a581f56eb
BNJ-Homme-Efficace <55634259+BNJ-Homme-Efficace@users.noreply.github.com>
2021-03-10 21:59:52 +0100

    Add files via upload

---
+++ bnj_loadingV1/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/music.mp3',
+    'html/css/style.css',
+    'html/css/reset.css',
+    'html/img/logo.png',
+    'html/img/flouville.png',
+    'html/js/jquery-2.1.1.js',
+    'html/js/main.js',
+    'html/js/app.js',
+    'html/js/modernizr.js'
+}
